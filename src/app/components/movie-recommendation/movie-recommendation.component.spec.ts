b7d98dadc13bfb79c30277ae35fd818ec1fabc19
Leon Ruwe <l.ruwe.adm@grimme.de>
2022-10-04 16:48:08 +0200

    fix(Test): mat autocomplete module added

--- src/app/components/movie-recommendation/movie-recommendation.component.spec.ts
+++ src/app/components/movie-recommendation/movie-recommendation.component.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 
@@ -14,7 +15,8 @@ describe('MovieRecommendationComponent', () => {
       imports: [
         MatInputModule,
         MatButtonModule,
-        HttpClientModule
+        HttpClientModule,
+        MatAutocompleteModule
       ],
       declarations: [ MovieRecommendationComponent ]
     })
